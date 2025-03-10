fe5a16f3fba8ba32d52bf3500264e003f15d537f
ArturDragunov98 <dragunovartur61@gmail.com>
2025-03-10 18:34:49 +0100

    day-26 added try/exception

--- day-26-NATO-alphabet/main.py
+++ day-26-NATO-alphabet/main.py
@@ -2,7 +2,25 @@ import pandas as pd
 df = pd.read_csv('nato_phonetic_alphabet.csv')
 
 phonetic_dict = {row.letter:row.code for (index,row) in df.iterrows()}
-word = input('Enter a word: ').upper()
-word_list = [x for x in word]
-output = [phonetic_dict[letter] for letter in word_list]
-print(output)
+# is_valid_word = False
+# while not is_valid_word: 
+#   word = input('Enter a word: ').upper()
+#   try:
+#     output = [phonetic_dict[letter] for letter in word]
+#     is_valid_word = True
+#   except KeyError:
+#     print('Only letters allowed')
+#   else: # will run when try statement succeeds. Correct approach of error handling.
+#     print(output)
+
+# Alternatively -> recursive function calling
+def generate_phonetic():
+  word = input('Enter a word: ').upper()
+  try:
+    output = [phonetic_dict[letter] for letter in word]
+  except KeyError:
+    print('Only letters allowed')
+    generate_phonetic()
+  else: # will run when try statement succeeds. Correct approach of error handling.
+    print(output)
+generate_phonetic()
